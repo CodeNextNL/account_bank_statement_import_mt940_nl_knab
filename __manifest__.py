7429a1e408219b55b4d4722dca43c6be829da865
wytzejan <wjriedstra@gmail.com>
2019-10-07 14:28:03 +0200

    migration to 12.0

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Import Knab T940 Bank Statements',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'codeNext, '
               'Odoo Community Association (OCA)',
